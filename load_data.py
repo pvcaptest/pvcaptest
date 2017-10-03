d279bc02c818d2934f5a88ffe1dc55ae15300639
bt- <benjaming.taylor@gmail.com>
2017-10-02 21:25:37 -0500

    re-organized load_data.py and add function to create pecos translation dictionary

--- load_data.py
+++ load_data.py
@@ -35,32 +35,6 @@ def load_das_data(path, filename):
     return(all_data)
 
 
-def series_type(series, type_defs, bounds_check=True, warnings=False):
-    for key in type_defs.keys():
-        # print('################')
-        # print(key)
-        for search_str in type_defs[key][0]:
-            # print(search_str)
-            if series.name.lower().find(search_str) == -1:
-                continue
-            else:
-                if bounds_check:
-                    min_bool = series.min() >= type_defs[key][1][0]
-                    max_bool = series.max() <= type_defs[key][1][1]
-                    if min_bool and max_bool:
-                        return key
-                    else:
-                        if warnings:
-                            if not min_bool:
-                                print('Values in {} exceed min values for {}'.format(series.name, key))
-                            elif not max_bool:
-                                print('Values in {} exceed max values for {}'.format(series.name, key))
-                        return key + '-valuesError'
-                else:
-                    return key
-    return ''
-
-
 aux_load = 100
 ac_nameplate = 21040
 
@@ -92,6 +66,32 @@ irr_sensors_defs = {'ref_cell': [['reference cell', 'reference', 'ref',
                     'pyran': [['pyranometer', 'pyran']]}
 
 
+def series_type(series, type_defs, bounds_check=True, warnings=False):
+    for key in type_defs.keys():
+        # print('################')
+        # print(key)
+        for search_str in type_defs[key][0]:
+            # print(search_str)
+            if series.name.lower().find(search_str) == -1:
+                continue
+            else:
+                if bounds_check:
+                    min_bool = series.min() >= type_defs[key][1][0]
+                    max_bool = series.max() <= type_defs[key][1][1]
+                    if min_bool and max_bool:
+                        return key
+                    else:
+                        if warnings:
+                            if not min_bool:
+                                print('Values in {} exceed min values for {}'.format(series.name, key))
+                            elif not max_bool:
+                                print('Values in {} exceed max values for {}'.format(series.name, key))
+                        return key + '-valuesError'
+                else:
+                    return key
+    return ''
+
+
 def equip_counts(df):
     equip_counts = {}
     eq_cnt_lst = []
@@ -112,6 +112,32 @@ def equip_counts(df):
             eq_cnt_lst.append(equip_counts[col_name])
 #         print(eq_cnt_lst[i])
     return eq_cnt_lst
+
+
+def trans_dict(df):
+    col_types = df.apply(series_type, args=(type_defs,)).tolist()
+    sub_types = df.apply(series_type, args=(sub_type_defs,),
+                         bounds_check=False).tolist()
+    irr_types = df.apply(series_type, args=(irr_sensors_defs,),
+                         bounds_check=False).tolist()
+
+    col_indices = []
+    for typ, sub_typ, irr_typ in zip(col_types, sub_types, irr_types):
+        col_indices.append('-'.join([typ, sub_typ, irr_typ]))
+
+    names = []
+    for new_name, old_name in zip(col_indices, df.columns.tolist()):
+        names.append((new_name, old_name))
+    names.sort()
+    orig_names_sorted = [name_pair[1] for name_pair in names]
+
+    trans = {}
+    for name in set(col_indices):
+        start = col_indices.index(name)
+        count = col_indices.count(name)
+        trans[name] = orig_names_sorted[start:start + count]
+
+    return trans
 
 
 def std_filter(series):
