d6f2aa28badadf5cfce9101a1e0de650e027f04a
bt- <benjaming.taylor@gmail.com>
2017-10-22 12:53:37 -0500

    Created test file for CapTest; no tests written.

---
+++ captest/tests/test_CapTest.py
@@ -0,0 +1,11 @@
+import unittest
+import captest as pvc
+
+class TestCapTest(unittest.TestCase):
+    """Tests for CapTest class."""
+
+    def test_filter_irr(self):
+        # column count shouldn't change
+        # min val after should be >= min
+        # max val after should be <= max
+        pass
