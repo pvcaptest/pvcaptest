e36b526f14ee6b5404cb01afa1725566f4c1ec85
bt- <benjaming.taylor@gmail.com>
2022-08-02 16:04:03 -0500

    Implements call and str dunder methods for ColumnGroups class

--- captest/columngroups.py
+++ captest/columngroups.py
@@ -6,11 +6,31 @@ class ColumnGroups(object):
     Parameters
     ----------
     column_groups : dict
+
+    Todo
+    ----
+    - Convert to a parametrized class with param library
+    - Create a function or method to create instance from excel file
+    - Create a function or method to create instance from json
+    - Create a function or method to create instance from yaml
+    - Create a function or method to create instance from DataFrame columns?
     """
     def __init__(self, column_groups):
         super(ColumnGroups, self).__init__()
         self.column_groups = column_groups
         self.assign_column_groups(column_groups)
+
+    def __call__(self):
+        return self.column_groups
+
+    def __str__(self):
+        """Print `column_groups` dictionary with nice formatting."""
+        output = ''
+        for grp_id, col_list in self.column_groups.items():
+            output += grp_id + ':\n'
+            for col in col_list:
+                output += ' ' * 4 + col + '\n'
+        return output
 
     def assign_column_groups(self, column_groups):
         for grp_id, cols in column_groups.items():
