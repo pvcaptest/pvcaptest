0c65d0c9e3dee38fbc6063505a6a0cc7a6c8884d
Ben Taylor <benjaming.taylor@gmail.com>
2023-04-06 19:23:07 -0500

    Black formatting

--- tests/test_prtest.py
+++ tests/test_prtest.py
@@ -17,43 +17,37 @@ To run a specific test:
 pytest tests/test_CapData.py::TestCapDataEmpty::test_capdata_empty
 """
 
-ix = pd.date_range(
-    start='1/1/2021 12:00',
-    freq='H',
-    periods=3
-)
+ix = pd.date_range(start="1/1/2021 12:00", freq="H", periods=3)
 
-ix_5min = pd.date_range(
-    start='1/1/2021 12:00',
-    freq='5min',
-    periods=3
-)
+ix_5min = pd.date_range(start="1/1/2021 12:00", freq="5min", periods=3)
 
-class TestGetCommonTimestep():
+
+class TestGetCommonTimestep:
     def test_output_type_str(self):
-        df = pd.DataFrame({'a':[1, 2, 4]}, index=ix)
-        time_step = pr.get_common_timestep(df, units='h', string_output=True)
+        df = pd.DataFrame({"a": [1, 2, 4]}, index=ix)
+        time_step = pr.get_common_timestep(df, units="h", string_output=True)
         assert isinstance(time_step, str)
 
     def test_output_type_numeric(self):
-        df = pd.DataFrame({'a':[1, 2, 4]}, index=ix)
-        time_step = pr.get_common_timestep(df, units='h', string_output=False)
+        df = pd.DataFrame({"a": [1, 2, 4]}, index=ix)
+        time_step = pr.get_common_timestep(df, units="h", string_output=False)
         assert isinstance(time_step, np.float64)
 
     def test_hours_string(self):
-        df = pd.DataFrame({'a':[1, 2, 4]}, index=ix)
-        time_step = pr.get_common_timestep(df, units='h', string_output=True)
-        assert time_step == '1.0 hours'
+        df = pd.DataFrame({"a": [1, 2, 4]}, index=ix)
+        time_step = pr.get_common_timestep(df, units="h", string_output=True)
+        assert time_step == "1.0 hours"
 
     def test_hours_numeric(self):
-        df = pd.DataFrame({'a':[1, 2, 4]}, index=ix)
-        time_step = pr.get_common_timestep(df, units='h', string_output=False)
+        df = pd.DataFrame({"a": [1, 2, 4]}, index=ix)
+        time_step = pr.get_common_timestep(df, units="h", string_output=False)
         assert time_step == 1.0
 
     def test_minutes_numeric(self):
-        df = pd.DataFrame({'a':[1, 2, 4]}, index=ix)
-        time_step = pr.get_common_timestep(df, units='m', string_output=False)
+        df = pd.DataFrame({"a": [1, 2, 4]}, index=ix)
+        time_step = pr.get_common_timestep(df, units="m", string_output=False)
         assert time_step == 60.0
+
 
 class TestTempCorrectPower:
     """Test correction of power by temperature coefficient."""
@@ -63,8 +57,7 @@ class TestTempCorrectPower:
 
     def test_output_type_series(self):
         assert isinstance(
-            pr.temp_correct_power(pd.Series([10, 12, 15]), -0.37, 50),
-            pd.Series
+            pr.temp_correct_power(pd.Series([10, 12, 15]), -0.37, 50), pd.Series
         )
 
     def test_high_temp_higher_power(self):
@@ -83,11 +76,7 @@ class TestTempCorrectPower:
         assert pytest.approx(corr_power, 0.3) == 11.019
 
     def test_math_series_power(self):
-        ix = pd.date_range(
-            start='1/1/2021 12:00',
-            freq='H',
-            periods=3
-        )
+        ix = pd.date_range(start="1/1/2021 12:00", freq="H", periods=3)
         power = pd.Series([10, 20, 15], index=ix)
         corr_power = pr.temp_correct_power(power, -0.37, 50)
         assert pytest.approx(corr_power.values, 0.3) == [11.019, 22.038, 16.528]
@@ -99,123 +88,95 @@ class TestTempCorrectPower:
         corr_power = pr.temp_correct_power(10, -0.37, 30, base_temp=27.5)
         assert pytest.approx(corr_power, 0.3) == 10.093
 
+
 class TestBackOfModuleTemp:
     """Test calculation of back of module (BOM) temperature from weather."""
+
     def test_float_inputs(self):
         assert pr.back_of_module_temp(800, 30, 3) == pytest.approx(48.1671)
 
     def test_series_inputs(self):
-        ix = pd.date_range(
-            start='1/1/2021 12:00',
-            freq='H',
-            periods=3
-        )
+        ix = pd.date_range(start="1/1/2021 12:00", freq="H", periods=3)
         poa = pd.Series([805, 810, 812], index=ix)
         temp_amb = pd.Series([26, 27, 27.5], index=ix)
         wind = pd.Series([0.5, 1, 2.5], index=ix)
 
-        exp_results = pd.Series(
-            [48.0506544,
-             48.3709869,
-             46.6442104],
-             index=ix
+        exp_results = pd.Series([48.0506544, 48.3709869, 46.6442104], index=ix)
+
+        assert (
+            pd.testing.assert_series_equal(
+                pr.back_of_module_temp(poa, temp_amb, wind), exp_results
+            )
+            is None
         )
 
-        assert pd.testing.assert_series_equal(
-            pr.back_of_module_temp(poa, temp_amb, wind),
-            exp_results
-        ) is None
-
     @pytest.mark.parametrize(
-        'racking, module_type, expected',
+        "racking, module_type, expected",
         [
-            ('open_rack', 'glass_cell_glass', 50.77154),
-            ('open_rack', 'glass_cell_poly', 48.33028),
-            ('open_rack', 'poly_tf_steel', 46.82361),
-            ('close_roof_mount', 'glass_cell_glass', 65.86252),
-            ('insulated_back', 'glass_cell_poly', 72.98647)
-        ]
+            ("open_rack", "glass_cell_glass", 50.77154),
+            ("open_rack", "glass_cell_poly", 48.33028),
+            ("open_rack", "poly_tf_steel", 46.82361),
+            ("close_roof_mount", "glass_cell_glass", 65.86252),
+            ("insulated_back", "glass_cell_poly", 72.98647),
+        ],
     )
     def test_emp_heat_coeffs(self, racking, module_type, expected):
         bom = pr.back_of_module_temp(
-            800,
-            28,
-            1.5,
-            module_type=module_type,
-            racking=racking
+            800, 28, 1.5, module_type=module_type, racking=racking
         )
         assert bom == pytest.approx(expected)
 
-class TestCellTemp():
+
+class TestCellTemp:
     def test_float_inputs(self):
         assert pr.cell_temp(30, 850) == pytest.approx(32.55)
 
     def test_series_inputs(self):
-        ix = pd.date_range(
-            start='1/1/2021 12:00',
-            freq='H',
-            periods=3
-        )
+        ix = pd.date_range(start="1/1/2021 12:00", freq="H", periods=3)
         poa = pd.Series([805, 810, 812], index=ix)
         temp_bom = pd.Series([26, 27, 27.5], index=ix)
 
-        exp_results = pd.Series(
-            [
-                28.415,
-                29.43,
-                29.936
-            ],
-             index=ix
-        )
+        exp_results = pd.Series([28.415, 29.43, 29.936], index=ix)
 
-        assert pd.testing.assert_series_equal(
-            pr.cell_temp(temp_bom, poa),
-            exp_results
-        ) is None
+        assert (
+            pd.testing.assert_series_equal(pr.cell_temp(temp_bom, poa), exp_results)
+            is None
+        )
 
     @pytest.mark.parametrize(
-        'racking, module_type, expected',
+        "racking, module_type, expected",
         [
-            ('open_rack', 'glass_cell_glass', 30.4),
-            ('open_rack', 'glass_cell_poly', 30.4),
-            ('open_rack', 'poly_tf_steel', 30.4),
-            ('close_roof_mount', 'glass_cell_glass', 28.8),
-            ('insulated_back', 'glass_cell_poly', 28)
-        ]
+            ("open_rack", "glass_cell_glass", 30.4),
+            ("open_rack", "glass_cell_poly", 30.4),
+            ("open_rack", "poly_tf_steel", 30.4),
+            ("close_roof_mount", "glass_cell_glass", 28.8),
+            ("insulated_back", "glass_cell_poly", 28),
+        ],
     )
     def test_emp_heat_coeffs(self, racking, module_type, expected):
-        bom = pr.cell_temp(
-            28,
-            800,
-            module_type=module_type,
-            racking=racking
-        )
+        bom = pr.cell_temp(28, 800, module_type=module_type, racking=racking)
         assert bom == pytest.approx(expected)
 
-class TestAvgTypCellTemp():
+
+class TestAvgTypCellTemp:
     def test_math(self):
-        ix = pd.date_range(
-            start='1/1/2021 12:00',
-            freq='H',
-            periods=3
-        )
+        ix = pd.date_range(start="1/1/2021 12:00", freq="H", periods=3)
         poa = pd.Series([805, 810, 812], index=ix)
         cell_temp = pd.Series([26, 27, 27.5], index=ix)
 
         assert pr.avg_typ_cell_temp(poa, cell_temp) == pytest.approx(26.8356)
 
 
-
-class TestCheckPerfRatioInputs():
+class TestCheckPerfRatioInputs:
     def test_ok_inputs(self):
-        ac_energy = pd.Series({'energy':[90, 95, 97]}, index=ix)
+        ac_energy = pd.Series({"energy": [90, 95, 97]}, index=ix)
         poa = pd.Series([805, 810, 812], index=ix)
         input_ok = pr.perf_ratio_inputs_ok(ac_energy, 110, poa)
         assert input_ok is True
 
     def test_warn_ac_energy_type(self):
         """Raise warning if `ac_energy` is not a Pandas Series."""
-        ac_energy = pd.DataFrame({'energy':[90, 95, 97]}, index=ix)
+        ac_energy = pd.DataFrame({"energy": [90, 95, 97]}, index=ix)
         poa = pd.Series([805, 810, 812], index=ix)
         with pytest.warns(UserWarning):
             input_ok = pr.perf_ratio_inputs_ok(ac_energy, 110, poa)
@@ -224,18 +185,14 @@ class TestCheckPerfRatioInputs():
     def test_warn_poa_type(self):
         """Raise warning if `poa` is not a Pandas Series."""
         ac_energy = pd.Series([90, 95, 97], index=ix)
-        poa = pd.DataFrame({'poa':[805, 810, 812]}, index=ix)
+        poa = pd.DataFrame({"poa": [805, 810, 812]}, index=ix)
         with pytest.warns(UserWarning):
             input_ok = pr.perf_ratio_inputs_ok(ac_energy, 110, poa)
         assert input_ok is False
 
     def test_poa_ac_energy_index_match(self):
         """Raise warning if indices of poa and ac_energy do not match."""
-        ix_poa = pd.date_range(
-            start='1/1/2021 13:00',
-            freq='H',
-            periods=3
-        )
+        ix_poa = pd.date_range(start="1/1/2021 13:00", freq="H", periods=3)
         ac_energy = pd.Series([90, 95, 97], index=ix)
         poa = pd.Series([805, 810, 812], index=ix_poa)
         with pytest.warns(UserWarning):
@@ -244,18 +201,15 @@ class TestCheckPerfRatioInputs():
 
     def test_avail_index_match(self):
         """Raise warning if index of availability does not match poa."""
-        ix_availability = pd.date_range(
-            start='1/1/2021 13:00',
-            freq='H',
-            periods=3
-        )
+        ix_availability = pd.date_range(start="1/1/2021 13:00", freq="H", periods=3)
         ac_energy = pd.Series([90, 95, 97], index=ix)
         poa = pd.Series([805, 810, 812], index=ix)
         avail = pd.Series([0.9, 1, 0.95], index=ix_availability)
         with pytest.warns(UserWarning):
             pr.perf_ratio(ac_energy, 110, poa, availability=avail)
 
-class TestPerfRatio():
+
+class TestPerfRatio:
     def test_simple_pr_hourly(self):
         """Test a short series of data for a hypothetical system.
 
@@ -264,10 +218,10 @@ class TestPerfRatio():
         - dc/ac ratio: 1.2
         - dc nameplate: 120 kW-DC
         """
-         # Wh for 3 hours
+        # Wh for 3 hours
         ac_energy = pd.Series([80_000, 90_000, 95_000], index=ix)
-        poa = pd.Series([850, 900, 1000], index=ix) # poa W/m^2
-        dc_nameplate = 120_000 #W-DC
+        poa = pd.Series([850, 900, 1000], index=ix)  # poa W/m^2
+        dc_nameplate = 120_000  # W-DC
 
         perf_ratio = pr.perf_ratio(ac_energy, dc_nameplate, poa)
         assert perf_ratio.pr <= 1
@@ -285,10 +239,10 @@ class TestPerfRatio():
         - dc/ac ratio: 1.2
         - dc nameplate: 120 kW-DC
         """
-         # kWh for 3 hours
+        # kWh for 3 hours
         ac_energy = pd.Series([80, 90, 95], index=ix)
-        poa = pd.Series([850, 900, 1000], index=ix) # poa W/m^2
-        dc_nameplate = 120_000 #W-DC
+        poa = pd.Series([850, 900, 1000], index=ix)  # poa W/m^2
+        dc_nameplate = 120_000  # W-DC
 
         perf_ratio = pr.perf_ratio(ac_energy, dc_nameplate, poa, unit_adj=1000)
         assert perf_ratio.pr <= 1
@@ -306,11 +260,11 @@ class TestPerfRatio():
         - dc/ac ratio: 1.2
         - dc nameplate: 120 kW-DC
         """
-         # Wh for 3 hours
+        # Wh for 3 hours
         ac_energy = pd.Series([80_000, 90_000, 95_000], index=ix_5min)
-        ac_energy = ac_energy / 12 # convert to Wh for 5min intervals
-        poa = pd.Series([850, 900, 1000], index=ix_5min) # poa W/m^2
-        dc_nameplate = 120_000 #W-DC
+        ac_energy = ac_energy / 12  # convert to Wh for 5min intervals
+        poa = pd.Series([850, 900, 1000], index=ix_5min)  # poa W/m^2
+        dc_nameplate = 120_000  # W-DC
 
         perf_ratio = pr.perf_ratio(ac_energy, dc_nameplate, poa)
         assert perf_ratio.pr <= 1
@@ -328,17 +282,12 @@ class TestPerfRatio():
         - dc/ac ratio: 1.2
         - dc nameplate: 120 kW-DC
         """
-         # Wh for 3 hours
+        # Wh for 3 hours
         ac_energy = pd.Series([80_000, 90_000, 95_000], index=ix)
-        poa = pd.Series([850, 900, 1000], index=ix) # poa W/m^2
-        dc_nameplate = 120_000 #W-DC
+        poa = pd.Series([850, 900, 1000], index=ix)  # poa W/m^2
+        dc_nameplate = 120_000  # W-DC
 
-        perf_ratio = pr.perf_ratio(
-            ac_energy,
-            dc_nameplate,
-            poa,
-            availability=0.9
-        )
+        perf_ratio = pr.perf_ratio(ac_energy, dc_nameplate, poa, availability=0.9)
         assert perf_ratio.pr == pytest.approx(0.8922556)
 
     def test_simple_pr_hourly_series_avail(self):
@@ -349,30 +298,25 @@ class TestPerfRatio():
         - dc/ac ratio: 1.2
         - dc nameplate: 120 kW-DC
         """
-         # Wh for 3 hours
+        # Wh for 3 hours
         ac_energy = pd.Series([80_000, 90_000, 95_000], index=ix)
-        poa = pd.Series([850, 900, 1000], index=ix) # poa W/m^2
-        dc_nameplate = 120_000 #W-DC
+        poa = pd.Series([850, 900, 1000], index=ix)  # poa W/m^2
+        dc_nameplate = 120_000  # W-DC
         avail = pd.Series([0.9, 1, 0.95], index=ix)
 
-        perf_ratio = pr.perf_ratio(
-            ac_energy,
-            dc_nameplate,
-            poa,
-            availability=avail
-        )
+        perf_ratio = pr.perf_ratio(ac_energy, dc_nameplate, poa, availability=avail)
         assert perf_ratio.pr == pytest.approx(0.844487)
 
     @pytest.mark.parametrize(
-        'degrad, year, expected',
+        "degrad, year, expected",
         [
             (0.5, 1, 0.807065),
             (0.5, 2, 0.811121),
             (0.5, 3, 0.815197),
             (0.7, 1, 0.808691),
             (0.7, 2, 0.814392),
-            (0.7, 3, 0.820132)
-        ]
+            (0.7, 3, 0.820132),
+        ],
     )
     def test_simple_pr_hourly_degrad(self, degrad, year, expected):
         """Test degradation applied to PR denominator.
@@ -382,22 +326,18 @@ class TestPerfRatio():
         - dc/ac ratio: 1.2
         - dc nameplate: 120 kW-DC
         """
-         # Wh for 3 hours
+        # Wh for 3 hours
         ac_energy = pd.Series([80_000, 90_000, 95_000], index=ix)
-        poa = pd.Series([850, 900, 1000], index=ix) # poa W/m^2
-        dc_nameplate = 120_000 #W-DC
+        poa = pd.Series([850, 900, 1000], index=ix)  # poa W/m^2
+        dc_nameplate = 120_000  # W-DC
 
         perf_ratio = pr.perf_ratio(
-            ac_energy,
-            dc_nameplate,
-            poa,
-            year=year,
-            degradation=degrad
+            ac_energy, dc_nameplate, poa, year=year, degradation=degrad
         )
         assert perf_ratio.pr == pytest.approx(expected)
 
 
-class TestPerfRatioTempCorrNREL():
+class TestPerfRatioTempCorrNREL:
     def test_simple_pr_hourly(self):
         """Test a short series of data for a hypothetical system.
 
@@ -406,19 +346,19 @@ class TestPerfRatioTempCorrNREL():
         - dc/ac ratio: 1.2
         - dc nameplate: 120 kW-DC
         """
-         # Wh for 3 hours
+        # Wh for 3 hours
         ac_energy = pd.Series([80_000, 90_000, 95_000], index=ix)
-        poa = pd.Series([850, 900, 1000], index=ix) # poa W/m^2
-        dc_nameplate = 120_000 #W-DC
+        poa = pd.Series([850, 900, 1000], index=ix)  # poa W/m^2
+        dc_nameplate = 120_000  # W-DC
         temp_amb = pd.Series([30, 32, 34], index=ix)
         wind_speed = pd.Series([1, 1.5, 0.8], index=ix)
         perf_ratio = pr.perf_ratio_temp_corr_nrel(
             ac_energy,
             dc_nameplate,
             poa,
-            power_temp_coeff = -0.37,
-            temp_amb = temp_amb,
-            wind_speed = wind_speed,
+            power_temp_coeff=-0.37,
+            temp_amb=temp_amb,
+            wind_speed=wind_speed,
         )
         assert perf_ratio.pr <= 1
         assert perf_ratio.pr > 0
