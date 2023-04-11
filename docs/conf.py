b2438d68a550f1ccf4f9d4e46f63512167cfdfa8
Ben Taylor <benjaming.taylor@gmail.com>
2023-04-11 13:32:57 -0500

    Adds additional paths to sys path

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,8 @@ import recommonmark
 from recommonmark.transform import AutoStructify
 
 sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../../'))
 
 
 # -- Project information -----------------------------------------------------
