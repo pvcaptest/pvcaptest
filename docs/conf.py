314cc3f3ff2a31babce16265d881355c7fb4d29a
Ben Taylor <benjaming.taylor@gmail.com>
2023-08-27 23:34:35 -0500

    Removes commented local vs rtd html theme

--- docs/conf.py
+++ docs/conf.py
@@ -89,19 +89,8 @@ except Exception:
 
 # -- Options for HTML output -------------------------------------------------
 
-
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-# # This code is copied from the pvlib package conf.py file
-
-# # on_rtd is whether we are on readthedocs.org
-# on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-
-# if not on_rtd:  # only import and set the theme if we're building docs locally
-#     import sphinx_rtd_theme
-#     html_theme = 'sphinx_rtd_theme'
-#     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-# else:
 html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
